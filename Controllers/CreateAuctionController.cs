185f0a56c6a68ef9dcc85d087d749944aa55fb63
Samuel Tegsten <samuel.tegsten@gmail.com>
2023-10-19 14:40:08 +0200

    added create auction page

---
+++ Controllers/CreateAuctionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAuctions.Core.Interfaces.Service;
+
+namespace WebAuctions.Controllers
+{
+    public class CreateAuctionController : Controller
+    {
+        private readonly IAuctionService auctionService;
+
+        public CreateAuctionController(IAuctionService auctionService)
+        {
+            this.auctionService = auctionService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
